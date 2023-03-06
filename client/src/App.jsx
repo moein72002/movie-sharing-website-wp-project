84333f4e7c06bd925fd0f6a5cf3dc8b34e2c1010
Moein Madadi <moeinmadadi2002@gmail.com>
2023-03-06 16:35:45 +0330

    add register page and add router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import './app.scss'
 import Home from "./pages/home/Home"
+import Register from "./pages/register/Register";
+import {
+    BrowserRouter, Route,
+    Routes
+} from "react-router-dom";
 
 const App = () => {
-  return <Home />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/register" element={<Register/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
