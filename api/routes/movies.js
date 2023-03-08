9d6bfd9a176eb1b14cb7f505f8393283652758c7
erfanasadi <erfan.asadi2272@gmail.com>
2023-03-08 17:31:03 +0330

    create movie , update movie api

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -68,3 +68,40 @@ router.get("/", verify, async (req, res) => {
 
 module.exports = router;
 
+//UPDATE
+
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovie = await Movie.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+      );
+      res.status(200).json(updatedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//DELETE
+
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await Movie.findByIdAndDelete(req.params.id);
+      res.status(200).json("The movie has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//GET
